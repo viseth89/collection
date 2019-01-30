b5a335ed527a9acb374fe05e9bbd4a7fb8e113d8
viseth89 <viseth89@gmail.com>
2019-01-30 09:49:45 -0600

    tweaking about

--- btre_pages/views.py
+++ btre_pages/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-
+from btre_listings.choices import price_choices, bedroom_choices, state_choices
+from btre_listings.models import Listing
+from btre_realtors.models import Realtor
 
 # Create your views here.
 def index(request):
-
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = { 
+        'listings':listings,
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+    
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
 
-    return render(request,'pages/about.html')
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors
+    }
+
+    return render(request,'pages/about.html', context)
